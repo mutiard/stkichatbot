d3e66d7dde441f82f21360780fc192e83b4280be
Mutia Rahmi Dewi <mutia15@mhs.if.its.ac.id>
2018-10-26 07:40:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     question = event.message.text
-    answer = request_api(question)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
+    if (question == 'halo'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='hai'))
+    else:
+        answer = request_api(question)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
 
 def request_api(question):
     url = api_url + api_port + api_route
